d4c89979dbb29b86c944ea41c5df9ebedc300a94
ibrahem-tantawy94 <ibrahem.tantawy94@gmail.com>
2022-02-06 00:02:39 +0200

    + adding new index function, adding new data balde blade file

--- app/Http/Controllers/GithubReposController.php
+++ app/Http/Controllers/GithubReposController.php
@@ -7,10 +7,26 @@ use Illuminate\Support\Facades\Http;
 
 class GithubReposController extends Controller
 {
+
     public function index()
     {
-        $response = Http::get('https://api.github.com/search/repositories?q=created:%3E2019-01-10&sort=stars&order=desc');
+        return view('data');
+    }
+    /**
+     * function that fetches the data from github repos endpoint
+     * 
+     * @param Request 
+     * @return json
+     */
+    public function fetchDataFromGithub(Request $request)
+    {
+        $query = $request->input('search');
+        $date = $request->input('date');
 
-        return $response->json();
+        $response = Http::get("https://api.github.com/search/repositories?q=created:%3E2019-01-10&sort=stars&order=desc");
+
+        return view('data', [
+            'items' => $response->json()['items']
+        ]);
     }
 }
---
+++ resources/views/data.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Corex Task - Ibrahem Tantawy</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        
+    </head>
+    <body>
+
+        <div class="my-5 d-flex justify-content-center">
+            <form class="form-inline" id="searchForm" action="/" method="POST">
+                @csrf
+                <div class="form-group">
+                    <input type="text" class="form-control" id="language" name="language" placeholder="Programing language">
+                    <input type="date" class="form-control" id="date" name="date" placeholder="date">
+                    <select class="custom-select ml-2" name="top">
+                        <option value="10">View the top 10</option>
+                        <option value="50">View the top 50</option>
+                        <option value="100">View the top 100</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+</body>
+
+</html>
